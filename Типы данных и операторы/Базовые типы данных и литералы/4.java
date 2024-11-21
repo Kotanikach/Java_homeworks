251b053409eb84697339be2442e50b7ecd9dba71
Kotanikach <133512784+Kotanikach@users.noreply.github.com>
2024-11-21 23:52:40 +0300

    Create 4.java

---
+++ Типы данных и операторы/Базовые типы данных и литералы/4.java
@@ -0,0 +1,8 @@
+public class MixedTypes {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        double result = a + b;
+        System.out.println("Результат сложения: " + result);
+    }
+}
