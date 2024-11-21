92a56cb653cb0dd41a8fc01df2c8f275e99240e2
Kotanikach <133512784+Kotanikach@users.noreply.github.com>
2024-11-21 23:54:26 +0300

    Create 5.java

---
+++ Типы данных и операторы/Базовые типы данных и литералы/5.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        float floatValue = 10.75f;
+        byte byteValue = (byte) floatValue;
+        System.out.println("Преобразованный byte: " + byteValue);
+    }
+}
