0748095c96ca2c5f00a71a8e3c7274d941be2845
Kotanikach <133512784+Kotanikach@users.noreply.github.com>
2024-11-21 23:49:38 +0300

    Create 2.java

---
+++ Типы данных и операторы/Базовые типы данных и литералы/2.java
@@ -0,0 +1,6 @@
+public class PrintName {
+    public static void main(String[] args) {
+        String name = "\u0415\u043A\u0430\u0442\u0435\u0440\u0438\u043D\u0430";
+        System.out.println(name);
+    }
+}
