4c6882ee88dde17f66e19a3dd94939ed284856db
Kotanikach <133512784+Kotanikach@users.noreply.github.com>
2024-11-27 22:48:01 +0300

    Create 1.java

---
+++ Наследование и полиморфизм/Классы и методы final/1.java
@@ -0,0 +1,23 @@
+/**
+ * Практика  #1 Приведите пример кода, демонстрирующего невозможность переопределять в подклассе final-метод суперкласса.
+ */
+
+class Parent {
+    public final void display() {
+        System.out.println("Это метод суперкласса.");
+    }
+}
+
+class Child extends Parent {
+    // Попытка переопределить метод display приведет к ошибке компиляции
+    public void display() {
+        System.out.println("Это метод подкласса.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.display(); // Это вызовет ошибку компиляции
+    }
+}
